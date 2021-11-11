0ac9cfdcbce4b521783e2ac6c5dda7b4061eecf1
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-11-11 12:35:29 +0100

    Added utility methods to main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,25 @@
 #include <lexer.h>
+#include <stdbool.h>
+#include <token_type.h>
+
+enum token_type current_token;
+bool has_error;
+
+void set_error(char* message);
+enum token_type get_token();
 
 int main() {
 	int r;
 	do {
-		r = yylex();
+		r = get_token();
 		printf("%d\n", r);
 	} while(r != 0);
 	return 0;
 }
+
+void set_error(char* message) {
+	has_error = true;
+	printf("%s\n", message);
+}
+
+enum token_type get_token() { return yylex(); }
