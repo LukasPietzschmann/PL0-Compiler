e98d325455e2e9e14375f76b95ad970214df0e08
Vincent Ugrai <ugrai.vincent@gmail.com>
2021-11-11 13:41:18 +0100

    implemented grammar rules

--- src/main.c
+++ src/main.c
@@ -57,3 +57,76 @@ bool match(enum token_type type) {
 	return false;
 }
 
+void get() {
+	match(GET);
+	assignment();
+}
+
+void post() {
+	match(POST);
+	expression();
+}
+
+void begin() {
+	match(BEG);
+	statement();
+	while(match(SEMICOLON)){
+		statement();
+	}
+	match(END);
+}
+
+void condition() {
+	match(IF);
+	comparison();
+	match(THEN);
+	statement();
+}
+
+void loop(){
+	match(WHILE);
+	condition();
+	match(DO);
+	statement();
+}
+
+void comparison() {
+	if(match(ODD))
+		expression();
+	else {
+		expression();
+		if(current_token == EQUAL || current_token == HASH || current_token == LESS || current_token == LESS_EQUAL ||
+				current_token == GREATER || current_token == GREATER_EQUAL) {
+			current_token = get_token();
+			expression();
+		}
+	}
+}
+
+void expression() {
+	if(current_token == PLUS || current_token == MINUS) {
+		current_token = get_token();
+		term();
+	}
+	while(current_token == PLUS || current_token == MINUS) {
+		current_token = get_token();
+		term();
+	}
+}
+
+void term() {
+	factor();
+	while(current_token == STAR || current_token == SLASH) {
+		current_token = get_token();
+		factor();
+	}
+}
+
+void factor() {
+	switch(current_token) {
+		case IDENT: match(IDENT); break;
+		case NUMBER: match(NUMBER); break;
+		case PAREN_OPEN: expression(); match(PAREN_CLOSE);
+		default: set_error("Unexpected Token"); break;
+	}
+}
