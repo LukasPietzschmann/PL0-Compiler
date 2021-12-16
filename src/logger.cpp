03ef6c08451fba35d9dfcc80710d95a87a1e77be
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-12-16 19:24:11 +0100

    Error messages are now printed on stderr

--- src/logger.cpp
+++ src/logger.cpp
@@ -4,5 +4,5 @@ bool has_error;
 
 void set_error(std::string_view message, const token& current_token) {
 	has_error = true;
-	std::cout << "[Line " << current_token.line_number << "] " << message << std::endl;
+	std::cerr << "[Line " << current_token.line_number << "] " << message << std::endl;
 }
