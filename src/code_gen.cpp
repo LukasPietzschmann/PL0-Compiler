d0da3e1bfeaff6f1babdd348ad9bfee63f5daed0
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-01-26 18:29:16 +0100

    More fixes in codegen

--- src/code_gen.cpp
+++ src/code_gen.cpp
@@ -1,135 +1,55 @@
 #include "code_gen.hpp"
 
 void dump_init(stmt_list::ptr start, std::ostream& os) {
-	COUT << "loadc 0" << std::endl;
+	COUT << "loadc " << context::the().lookup_procedure(0).number_of_variables + 2 << " # Init Start" << std::endl;
 	COUT << "storer 0" << std::endl;
 	COUT << "loadc 0" << std::endl;
-	COUT << "jump " << start.get() << std::endl;
+	COUT << "jump " << start.get() << " # Init End" << std::endl;
 }
 
+/**
+ * Number of variables on stack
+ */
 void dump_ram_up(std::ostream& os) {
-	// delta liegt über var_count auf dem stack
 	os << "ram_up	nop" << std::endl;
 
-	// sl
-	COUT << "loadr 0" << std::endl;	 // TOS backup
-	COUT << "swap" << std::endl;
-	COUT << "loadr 0" << std::endl;	 // int v = m_elements[0];
-	COUT << "swap" << std::endl;
-	COUT << "loadc 0" << std::endl;
-	COUT << "storer 0" << std::endl;  // i wird gespeichert
-	COUT << "dup" << std::endl;	 // i wird gespeichert
-
-	/*
-		delta
-		delta
-		v
-		back
-		vc
-	 */
-
-	os << "for_loop_start	nop" << std::endl;
-	COUT << "loadr 0" << std::endl;	 // lade i
-	COUT << "swap" << std::endl;
-	COUT << "cmplt" << std::endl;
-	COUT << "jumpz for_loop_end" << std::endl;
-	COUT << "swap" << std::endl;
-	/*
-		v
-		delta
-		back
-		vc
-	 */
-	COUT << "loadr 0" << std::endl;	 // i
-	COUT << "loadc 1" << std::endl;
-	COUT << "add" << std::endl;	 // i + 1
-	COUT << "storer 0" << std::endl;  // neues i speichern
-	/*
-		v
-		delta
-		back
-		vc
-	 */
-	COUT << "loads" << std::endl;
-	COUT << "swap" << std::endl;
+	COUT << "loadr 0" << std::endl;
+	COUT << "add" << std::endl;
+	COUT << "inc 2" << std::endl;
 	COUT << "dup" << std::endl;
-	COUT << "jump for_loop_start" << std::endl;
-	os << "for_loop_end	nop" << std::endl;
-	/*
-		delta
-		delta
-		v
-		back
-		vc
-	 */
-	COUT << "drop" << std::endl;
-	COUT << "drop" << std::endl;
-	COUT << "swap" << std::endl;
-	COUT << "storer 0" << std::endl;  // backup zurück speichern
-	// hier sollte v ganz oben auf dem stack sein
-
-	/*
-		elements[v]
-		vc
-	 */
-	COUT << "swap" << std::endl;
-
-	// dl
+	COUT << "dec 1" << std::endl;
 	COUT << "loadr 0" << std::endl;
 	COUT << "swap" << std::endl;
-
-	// new_tos
-	COUT << "loadr 0" << std::endl;
-	COUT << "swap" << std::endl;
-	COUT << "loadc 2" << std::endl;
-	COUT << "swap" << std::endl;
-	COUT << "add" << std::endl;
-	COUT << "add" << std::endl;
-	COUT << "storer " << 0 << std::endl;
-
-	// hier sollte dl ganz oben auf dem stack sein
-	// m_elements[new_tos - 1] = dl;
-	COUT << "loadr 0" << std::endl;
-	COUT << "loadc 1" << std::endl;
-	COUT << "sub" << std::endl;
 	COUT << "stores" << std::endl;
-
-	// hier sollte v ganz oben auf dem stack sein
-	COUT << "loadr 0" << std::endl;
+	COUT << "dup" << std::endl;
+	COUT << "storer 0" << std::endl;
 	COUT << "stores" << std::endl;
-
 	COUT << "return" << std::endl;
 }
 
 void dump_ram_down(std::ostream& os) {
 	os << "ram_down	nop" << std::endl;
 
-	COUT << "loadc 1" << std::endl;
 	COUT << "loadr 0" << std::endl;
-	COUT << "sub" << std::endl;
+	COUT << "dec 1" << std::endl;
 	COUT << "loads" << std::endl;
 	COUT << "storer 0" << std::endl;
 	COUT << "return" << std::endl;
 }
 
 /**
- * Lesezugriff: danach loads
- * Schreibzugriff: vorher wert auf den stack, danach stores
+ * Read: Call `loads` afterwards
+ * Write: Put new value on stack before and call `stores` afterwards
  */
 void dump_var_address(int delta, int id, std::ostream& os) {
-	COUT << "loadr 0" << std::endl;
+	COUT << "loadr 0 # Get Var Address Begin" << std::endl;
 	for(int i = 0; i < delta; ++i)
-		COUT << "loads 0" << std::endl;
-	COUT << "loadc 2" << std::endl;
-	COUT << "sub" << std::endl;
-	COUT << "loadc " << id << std::endl;
-	COUT << "sub" << std::endl;
+		COUT << "loads" << std::endl;
+	COUT << "dec " << 2 + id << " # Get Var Address End" << std::endl;
 }
 
 void gen(stmt_list::ptr stmt, std::ostream& os) {
 	assert(stmt != nullptr);
-
-	int sf;
 
 	os << stmt.get() << "	nop" << std::endl;
 
@@ -142,7 +62,6 @@ void gen(stmt_list::ptr stmt, std::ostream& os) {
 		case stmt_list::t_call:
 			COUT << "loadc " << context::the().lookup_procedure(stmt->get_position().id).number_of_variables
 				 << std::endl;
-			COUT << "loadc " << stmt->get_position().delta << std::endl;
 			COUT << "call ram_up" << std::endl;
 			COUT << "call " << context::the().lookup_procedure(stmt->get_position().id).procedure.get() << std::endl;
 			COUT << "call ram_down" << std::endl;
@@ -164,7 +83,7 @@ void gen(stmt_list::ptr stmt, std::ostream& os) {
 		case stmt_list::t_end: COUT << "return" << std::endl; break;
 		case stmt_list::t_nop: COUT << "nop" << std::endl; break;
 	}
-	if(stmt_list::ptr next = stmt->get_next(); next != nullptr)
+	if(stmt_list::ptr next = stmt->get_next(); next != nullptr && stmt->get_type() != stmt_list::t_jmp)
 		gen(next, os);
 }
 
