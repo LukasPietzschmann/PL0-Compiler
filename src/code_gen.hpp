15c97a6ba2f9ed83a95e935e26d6544126c00c34
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-02-09 15:18:57 +0100

    Corrected formatting

--- src/code_gen.hpp
+++ src/code_gen.hpp
@@ -9,19 +9,19 @@
 #define COUT m_os << "\t"
 
 class code_gen {
-	public:
-		code_gen(const stmt_list::ptr& start, std::ofstream os);
+public:
+	code_gen(const stmt_list::ptr& start, std::ofstream os);
 
-		void dump_calc_new_sl(int delta);
-		void dump_var_address(int delta, int id);
+	void dump_calc_new_sl(int delta);
+	void dump_var_address(int delta, int id);
 
-		void gen(const stmt_list::ptr& stmt);
-		void gen(const expr_tree::ptr& expr);
+	void gen(const stmt_list::ptr& stmt);
+	void gen(const expr_tree::ptr& expr);
 
-	private:
-		std::ofstream m_os;
+private:
+	std::ofstream m_os;
 
-		void dump_init(const stmt_list::ptr& start);
-		void dump_ram_up();
-		void dump_ram_down();
+	void dump_init(const stmt_list::ptr& start);
+	void dump_ram_up();
+	void dump_ram_down();
 };
